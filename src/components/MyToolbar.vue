4e737a3ee816c50b1bbcd351f3a070c548b477de
z-haopeng <kevinzhou2222@gmail.com>
2022-11-18 15:07:56 -0500

    Add toolbar

---
+++ src/components/MyToolbar.vue
@@ -0,0 +1,20 @@
+<template>
+    <nav>
+        <v-toolbar color="indigo">
+            <v-toolbar-title class="text-uppercase">
+                <v-icon start>mdi-vuetify</v-icon>
+                Frameworks
+            </v-toolbar-title>
+            <v-btn elevation="3">
+                <v-icon start>mdi-plus-circle</v-icon>
+                Add
+            </v-btn>
+        </v-toolbar>
+    </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
-  <HelloWorld />
+  <MyToolbar />
 </template>
 
 <script>
 import { defineComponent } from 'vue';
 
 // Components
-import HelloWorld from '../components/HelloWorld.vue';
+import MyToolbar from '@/components/MyToolbar.vue';
 
 export default defineComponent({
   name: 'HomeView',
 
   components: {
-    HelloWorld,
+    MyToolbar,
   },
 });
 </script>
