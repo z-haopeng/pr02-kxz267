7d220f4bdb2c96e06331bd8ea451becafed940a4
z-haopeng <kevinzhou2222@gmail.com>
2022-11-20 11:42:16 -0500

    Build for production

---
+++ dist/js/about.358b605b.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkpr02_kxz267"]=self["webpackChunkpr02_kxz267"]||[]).push([[443],{6506:function(n,r,u){u.r(r),u.d(r,{default:function(){return k}});var t=u(3396);const a={class:"about"},e=(0,t._)("h1",null,"This is an about page",-1),s=[e];function c(n,r){return(0,t.wg)(),(0,t.iD)("div",a,s)}var i=u(89);const o={},f=(0,i.Z)(o,[["render",c]]);var k=f}}]);
+//# sourceMappingURL=about.358b605b.js.map
