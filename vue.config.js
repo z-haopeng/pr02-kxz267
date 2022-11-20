fda08560e5aabc6c0178ff2a95bb305558997574
z-haopeng <kevinzhou2222@gmail.com>
2022-11-20 12:05:36 -0500

    Build production for real this time

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === "production" ? "/pr02-kxz267/" : "/",
+  outputDir: 'docs',
 
   pluginOptions: {
     vuetify: {
